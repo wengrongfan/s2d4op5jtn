6ba4f4e01c8827f86a96d653917965e91ed235a8
wengrongfan <475827654@qq.com>
2017-09-17 02:16:35 +0800

    修改前端

---
+++ axfk-news/head.php
@@ -0,0 +1,18 @@
+<!--头部-->
+<div class="header">
+    <div class="head clearfix">
+        <div class="logo"><a href=""><img src="Assets/images/logo2.jpg" alt="云秀国际"/></a></div>
+        <div class="nav_m">
+            <div class="n_icon">导航栏</div>
+            <ul class="nav clearfix">
+                <li><a href="index.html">首页</a></li>
+                <li><a href="about.html">关于云秀</a></li>
+                <li><a href="#">云秀团队</a></li>
+                <li class="now"><a href="case.html">新闻动态</a></li>
+                <li><a href="product.html">云秀服务</a></li>
+                <li><a href="contact.html">联系我们</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!--头部-->
