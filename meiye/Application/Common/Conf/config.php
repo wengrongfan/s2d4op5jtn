0da5898eb70f66ab75e6210b0f1fc063e3f66ffe
wengrongfan <475827654@qq.com>
2017-12-24 15:17:45 +0800

    删除没用

--- meiye/Application/Common/Conf/config.php
+++ meiye/Application/Common/Conf/config.php
@@ -1,10 +1,10 @@
 <?php
 return array(
 'DB_TYPE'   => 'mysql', // 数据库类型
-'DB_HOST'   => 'localhost', // 数据库连接地址
+'DB_HOST'   => '111.230.147.181', // 数据库连接地址
 'DB_NAME'   => 'yunxu', // 数据库名
-'DB_USER'   => 'root', // 数据库用户名
-'DB_PWD'    => '', // 数据库密码
+'DB_USER'   => 'yunxiu', // 数据库用户名
+'DB_PWD'    => 'yunxiu9527', // 数据库密码
 'DB_PORT'   => 3306, // 数据库端口
 'DB_PREFIX' => 'mr_', // 数据库前缀 
 'DB_CHARSET'=> 'utf8', // 数据库编码
